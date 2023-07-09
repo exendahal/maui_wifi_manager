b06971d9e5fb1380e77b6a97dcc479911797480c
Santosh Dahal <exendahal@gmail.com>
2023-07-09 18:29:03 +0545

    feat: add init method on Android

--- Demo/Platforms/Android/MainActivity.cs
+++ Demo/Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.MauiWifiManager;
 
 namespace Demo
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public static Context AppContext { get; private set; }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AppContext = ApplicationContext;
+            WifiNetworkService.Init(this);
+        }
     }
 }
--- XFWifiManager/WiFiNetworkService.android.cs
+++ XFWifiManager/WiFiNetworkService.android.cs
@@ -1,36 +1,88 @@
-﻿using Plugin.MauiWifiManager.Abstractions;
+﻿using Android.App;
+using Android.Content;
+using Android.Net.Wifi;
+using Android.OS;
+using Plugin.MauiWifiManager.Abstractions;
 using System;
 using System.Threading.Tasks;
+using static Android.Provider.Settings;
+using Context = Android.Content.Context;
 
 namespace Plugin.MauiWifiManager
 {
     /// <summary>
     /// Interface for WiFiNetworkService
     /// </summary>
+    /// 
     public class WifiNetworkService : IWifiNetworkService
     {
+        private static Context _context;
         public WifiNetworkService() 
-        { 
-        }
+        {
             
+        }
+
+        public static void Init(Context context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context), "Please call WifiNetworkService.Init(this) inside the MainActivity's OnCreate function.");
+            _context = context;
+        }
+        /// <summary>
+        /// Connect Wi-Fi
+        /// </summary>
         public Task<NetworkData> ConnectWifi(string ssid, string password)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Disconnect Wi-Fi
+        /// From Android Q (Android 10) you can't enable/disable wifi programmatically anymore. 
+        /// So, use Settings Panel to toggle wifi connectivity
+        /// </summary>
         public void DisconnectWifi(string ssid)
         {
-            throw new NotImplementedException();
-        }       
+            if (_context == null)
+                throw new ArgumentNullException(nameof(_context), "Please call WifiNetworkService.Init(this) inside the MainActivity's OnCreate function.");
+           
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Q)
+            {
+                Intent panelIntent = new Intent(Panel.ActionWifi);
+                _context.StartActivity(panelIntent);
+            }               
+            else
+            {
+                WifiManager wifiManager = (WifiManager)Application.Context.GetSystemService(Context.WifiService);
+                wifiManager.SetWifiEnabled(false); // Disable wifi
+                wifiManager.SetWifiEnabled(true); // Enable wifi
+            }
+        }
 
+        /// <summary>
+        /// Get Wi-Fi Network Info
+        /// </summary>
         public Task<NetworkData> GetNetworkInfo()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Open Wi-Fi Setting
+        /// </summary>
         public Task<bool> OpenWifiSetting()
         {
-            throw new NotImplementedException();
+            if (_context == null)
+                throw new ArgumentNullException(nameof(_context), "Please call WifiNetworkService.Init(this) inside the MainActivity's OnCreate function.");
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+            Intent panelIntent;
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Q)
+                panelIntent = new Intent(Panel.ActionWifi);
+            else
+                panelIntent = new Intent(ActionWifiSettings);
+            _context.StartActivity(panelIntent);
+            taskCompletionSource.TrySetResult(true);
+            return taskCompletionSource.Task;
         }
 
         /// <summary>
